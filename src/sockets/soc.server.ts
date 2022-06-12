5e63f4b9bb598a56883a9fc7e78a79d2288c36ba
hrithikcse52 <hrithik.cse52@gmail.com>
2022-06-12 14:55:29 +0530

    refactor: sockets and conversation sort

--- src/sockets/soc.server.ts
+++ src/sockets/soc.server.ts
@@ -1,15 +1,21 @@
 import { Namespace } from 'socket.io';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
+import { getSocketRoom } from '../utils/index.utils';
 
 export function initializeSocket(msgSoc: Namespace<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) {
   msgSoc.on('connection', (socket) => {
     const { username, id } = socket.handshake.query;
     console.log('room: ', `${username}_${id}`);
-    socket.join(`${username}_${id}`);
-    console.log('consection sec', username, id);
-    socket.on('disconnect', (args) => {
-      console.log('discon args', args);
-      socket.leave(`${username}_${id}`);
-    });
+    console.log('rooms:', socket.rooms);
+    if (username && id) {
+      const roomName = getSocketRoom(username as string, id as string);
+      socket.join(roomName);
+      socket.broadcast.emit('user_active', { username, id });
+      console.log('consection sec', username, id);
+      socket.on('disconnect', (args) => {
+        console.log('discon args', args);
+        socket.leave(roomName);
+      });
+    }
   });
 }
